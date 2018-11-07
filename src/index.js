07d43c81d85a26d55885ae6d0f13202631f0a67f
Nicholas Chen <nichwch@gmail.com>
2018-11-07 00:57:43 -0800

    building the switch button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import './App.css';
+
+function Title(props) {
+  return (<div>
+            <h1>breadhaiku</h1>
+            <span><Buttons writing = {props.writing} switch = {props.switch}/></span>
+          </div>
+  )
+
+};
+
+function Buttons(props){
+  //if app is in browse mode, show the write button
+  if(props.writing == false)
+  {
+    return(
+      <span>
+      <p>
+        browse
+      </p>
+      <button onClick = {props.switch}>
+      <p>
+        //write
+      </p>
+      </button>
+      </span>
+    )
+  }
+  //if app is in writing mode, show the browse button
+  else{
+    return(
+      <span>
+      <button onClick = {props.switch}>
+      <p>
+        browse
+      </p>
+      </button>
+      <p>
+        //write
+      </p>
+      </span>
+    )
+  }
+}
+
+class App extends Component {
+  constructor()
+  {
+    super();
+    this.switchMode = this.switchMode.bind(this);
+    this.state = {
+      writing:false,
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Title writing={this.state.writing}
+               switch = {this.switchMode}
+        />
+      </div>
+    );
+  }
+
+  switchMode() {
+    if(this.state.writing == false)
+    {
+      this.setState({writing:true});
+    }
+    else {
+      this.setState({writing:false});
+    }
+
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
