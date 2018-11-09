5a11ebd3fbad1deda5f210ee94d42c70f4f7ee21
Nicholas Chen <nichwch@gmail.com>
2018-11-08 21:55:44 -0800

    added browse tab

--- src/index.js
+++ src/index.js
@@ -49,13 +49,17 @@ class InputField extends Component {
     super();
     this.state = {
       writtenpoem:{
-        line1:"",
-        line2:"",
-        line3:"",
-        author:"",
+        line1:"a",
+        line2:"a",
+        line3:"a",
+        author:"a",
       }
     }
-
+    this.handleChange1 = this.handleChange1.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
+    this.handleChange3 = this.handleChange3.bind(this);
+    this.handleChange4 = this.handleChange4.bind(this);
+    this.submitChange = this.submitChange.bind(this);
   }
 
   render() {
@@ -88,13 +92,54 @@ class InputField extends Component {
         placeholder="your name"
         type="text" />
       </form>
-      <button>
+      <button onClick = {this.submitChange}>
         submit
       </button>
       </div>)
     }
   }
+
+  handleChange1(event){
+    this.setState({input1: event.target.value});
+  }
+  handleChange2(event){
+    this.setState({input2: event.target.value});
+  }
+  handleChange3(event){
+    this.setState({input3: event.target.value});
+  }
+  handleChange4(event){
+    this.setState({input4: event.target.value});
+  }
+  submitChange(){
+    this.props.submit({
+      line1: this.state.input1,
+      line2: this.state.input2,
+      line3: this.state.input3,
+      author: this.state.input4,
+    })
+  }
 }
+
+  function Feed(props) {
+    if(props.writing == false)
+    {
+      let poems = props.poemList.map(poem => {
+        return (
+          <li key={poem.line1}>
+            <p>{poem.line1}</p>
+            <p>{poem.line2}</p>
+            <p>{poem.line3}</p>
+            <p>Written by {poem.author}</p>
+          </li>
+        )
+      })
+      return (<ul>
+        {poems}
+      </ul>)
+    }
+    return null;
+  }
 
 class App extends Component {
   constructor()
@@ -103,7 +148,7 @@ class App extends Component {
     this.switchMode = this.switchMode.bind(this);
     this.state = {
       writing:true,
-      poemlist:[
+      poemList:[
         {
           line1:"bread",
           line2:"is",
@@ -122,9 +167,10 @@ class App extends Component {
           line3:"good",
           author:"nick",
         },
-
       ]
     };
+
+    this.submitHaiku = this.submitHaiku.bind(this);
   }
   render() {
     return (
@@ -132,9 +178,17 @@ class App extends Component {
         <Title writing={this.state.writing}
                switch = {this.switchMode}
         />
-        <InputField writing={this.state.writing} />
+        <InputField writing={this.state.writing}
+                    submit = {this.submitHaiku}/>
+        <Feed writing={this.state.writing}
+              poemList = {this.state.poemList}/>
       </div>
     );
+  }
+  submitHaiku(newHaiku) {
+    console.log(this.state.poemList);
+    let newPoemList = this.state.poemList.concat(newHaiku);
+    this.setState({poemList: newPoemList})
   }
 
   switchMode() {
