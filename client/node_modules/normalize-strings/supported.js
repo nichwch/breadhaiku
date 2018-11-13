7acb5858bba2bd7cad2071af2faef2fe9a8ef378
Nicholas Chen <nichwch@gmail.com>
2018-11-13 15:56:40 -0800

    gitignore

--- client/node_modules/normalize-strings/supported.js
+++
@@ -1,9 +0,0 @@
-var charmap = require('./charmap');
-
-var supported_chars = [];
-
-Object.keys(charmap).forEach(function(code) {
-  supported_chars.push(String.fromCharCode(code));
-});
-
-console.log(supported_chars.join(' '));
